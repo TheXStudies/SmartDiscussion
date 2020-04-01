403d6e0e1ed7012f4778dfb63447dafd64c27c7a
Andrey Y <thexstudies@gmail.com>
2020-04-01 21:18:26 +0300

    Add `User` and `Article` models

--- smart_discussion/models.py
+++ smart_discussion/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class User(models.Model):
     nickname = models.CharField(max_length=64)
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
