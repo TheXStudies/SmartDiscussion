427c706bf1748211e5073cad66e224164f936dda
Andrey <ayanov@deloitte.com>
2020-04-06 12:55:33 +0300

    Add logged in user as author of posted article

--- smart_discussion/models.py
+++ smart_discussion/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
-
-class User(models.Model):
-    nickname = models.CharField(max_length=64)
+from django.contrib.auth import get_user_model
 
 
 class Article(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=128)
     text = models.TextField()
 
+
+class Comment(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
--- smart_discussion/views.py
+++ smart_discussion/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
+from django.views.generic.edit import ModelFormMixin
+from django.forms.widgets import HiddenInput
 
 from smart_discussion.models import Article
 
@@ -17,7 +19,27 @@ def logout(request, next_page='index'):
 
 
 class ArticleCreate(CreateView):
+    class Meta:
+        widgets = {
+            'author': HiddenInput(),
+        }
+
     success_url = "/"
     template_name = "new_article.html"
     model = Article
     fields = ['title', 'text']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.save()
+        return super(ModelFormMixin, self).form_valid(form)
+
+    def get_form_kwargs(self):
+        form_kwargs = super().get_form_kwargs()
+        # if not form_kwargs['instance']:
+        #     form_kwargs.setdefault('initial', {})['author'] = self.request.user
+        # else:
+        #     form_kwargs.setdefault('data', {})['author'] = self.request.user
+        #
+        return form_kwargs
